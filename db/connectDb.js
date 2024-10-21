cf2bbfd7b8b98e1f868c4e960ce27e8c7b814326
pala sharon kumar <163455102+sharonkumar1000@users.noreply.github.com>
2024-10-22 00:32:39 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(`mongodb+srv://sharonkumarpala:sharon1234@cluster0.czoxg.mongodb.net/chai`, {
-        useNewUrlParser: true,
+        useUnifiedTopology: true,
       });
     } catch (error) {
       process.exit(1);
