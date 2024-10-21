32a7445e478f1c2db33e40d6e3dc7cdfffd919b6
pala sharon kumar <163455102+sharonkumar1000@users.noreply.github.com>
2024-10-21 19:36:33 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/chai`, {
+      const conn = await mongoose.connect(`mongodb+srv://sharonkumarpala:sharon1234@cluster0.czoxg.mongodb.net/chai`, {
         useNewUrlParser: true,
       });
       console.log(`MongoDB Connected: {conn.connection.host}`);
