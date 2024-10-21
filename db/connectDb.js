92088f7f3552e990989ae5d0778262da89f3672b
pala sharon kumar <163455102+sharonkumar1000@users.noreply.github.com>
2024-10-22 00:37:41 +0530

    Update connectDb.js

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb+srv://sharonkumarpala:sharon1234@cluster0.czoxg.mongodb.net/chai`;
+      const conn = await mongoose.connect(`mongodb+srv://sharonkumarpala:sharon1234@cluster0.czoxg.mongodb.net/chai`);
     } catch (error) {
       process.exit(1);
     }
