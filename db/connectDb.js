3e8f3ac092aa452d1885c77ac077f50665726a38
pala sharon kumar <163455102+sharonkumar1000@users.noreply.github.com>
2024-10-21 23:47:35 +0530

    Update connectDb.js
    after removing console.log

--- db/connectDb.js
+++ db/connectDb.js
@@ -5,9 +5,7 @@ const connectDB = async () => {
       const conn = await mongoose.connect(`mongodb+srv://sharonkumarpala:sharon1234@cluster0.czoxg.mongodb.net/chai`, {
         useNewUrlParser: true,
       });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
     } catch (error) {
-      console.error(error.message);
       process.exit(1);
     }
   }
